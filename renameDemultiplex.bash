688041d88cd917eb9b250a0b570fe267a4270a97
Chris Bird <cbird808@gmail.com>
2019-09-17 14:51:44 -0500

    update

--- renameDemultiplex.bash
+++ renameDemultiplex.bash
@@ -2,9 +2,9 @@
 # This script should be run line by line, first deleting the `-i` option of sed, until desired output is confirmed
 
 # update these variables as neccessary
-SpCODE=Sde
+SpCODE=Aur
 PREFIX=PIRE2019
-SITE=Mat
+SITE=Rag
 
 # make sure that files don't have CRLF
 ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -q sed -i 's/\r$//g'
@@ -22,10 +22,10 @@ ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -j12 -q sed -i "s/\t$SpCOD
 ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -j12 -q sed -i "s/\($SpCODE-[AC]\)/\1-/" {} 
 
 # add _ after the SITE
-ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -j12 -q sed -i "s/\($SITE\)/\1_/" {}
+#ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -j12 -q sed -i "s/\($SITE\)/\1_/" {}
 
-#make replacement suffix
-ls *${SpCODE}*_demultiplex.txt | sed 's/_demultiplex.txt//' | sed "s/PI_$SpCODE\([123456789]\)/\1/" | \
+#make replacement suffix, this one needs troublshooting every time because of inconsistent naming
+ls *${SpCODE}*_demultiplex.txt | sed 's/_demultiplex.txt//' | sed "s/_$SpCODE\([123456789]\)/-\1/" | \
 sed "s/20190905[_-]PIRE[_-]$SpCODE-[AC][_-]//" | sed 's/^P/Plate/' | sed 's/1P/1Pool/' | sed 's/_/Seq1-/' | \
 sed 's/$/-L4/' > ${SpCODE}_lab.txt
 
@@ -35,8 +35,8 @@ ls *${SpCODE}*_demultiplex.txt > ${SpCODE}_dmx.txt
 #add lab info
 parallel --no-notice --link sed -i "s/$/-{2}/" {1} :::: ${SpCODE}_dmx.txt :::: ${SpCODE}_lab.txt
 
-#add bandaid to separate plate
-ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -j12 "sed -i 's/_Plate/-Plate/' {} "
+#add bandaid to add Seq
+ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -j12 "sed -i 's/\(Pool[0-9][0-9]*\)-/\1Seq1-/' {} "
 
 # fix pools by replacing -pool with _pool
 ls *${SpCODE}*_demultiplex.txt | parallel --no-notice -j12 "sed -i 's/-Pool-/_Pool-/' {} "
